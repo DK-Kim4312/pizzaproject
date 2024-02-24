6ef13ab7af5a08f46a210d7b97e931a745b31874
GeWangzi <56490458+GeWangzi@users.noreply.github.com>
2024-02-24 15:11:29 -0600

    updates to map, south American cities

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { ComposableMap, Geographies, Geography, ZoomableGroup } from "react-simple-maps";
+import { ComposableMap, Marker, Geographies, Geography, ZoomableGroup } from "react-simple-maps";
 import Sidebar from './Sidebar/Sidebar';
 import "./index.css";
 
 const getRandom = () => Math.random() * 0.8;
+
+const markers = [
+  {
+    markerOffset: 1,
+    name: "Buenos Aires",
+    coordinates: [-58.3816, -34.6037]
+  },
+  { markerOffset: 5, name: "La Paz", coordinates: [-68.1193, -16.4897] },
+  { markerOffset: 5, name: "Brasilia", coordinates: [-47.8825, -15.7942] },
+  { markerOffset: 5, name: "Santiago", coordinates: [-70.6693, -33.4489] },
+  { markerOffset: 5, name: "Bogota", coordinates: [-74.0721, 4.711] },
+  { markerOffset: 5, name: "Quito", coordinates: [-78.4678, -0.1807] },
+  { markerOffset: 5, name: "Georgetown", coordinates: [-58.1551, 6.8013] },
+  { markerOffset: 5, name: "Asuncion", coordinates: [-57.5759, -25.2637] },
+  { markerOffset: 5, name: "Paramaribo", coordinates: [-55.2038, 5.852] },
+  { markerOffset: 5, name: "Montevideo", coordinates: [-56.1645, -34.9011] },
+  { markerOffset: 5, name: "Caracas", coordinates: [-66.9036, 10.4806] },
+  { markerOffset: 5, name: "Lima", coordinates: [-77.0428, -12.0464] }
+];
 
 const App = () => (
   <div>
@@ -35,6 +54,18 @@ const App = () => (
             ))
           }
         </Geographies>
+        {markers.map(({ name, coordinates, markerOffset }) => (
+        <Marker key={name} coordinates={coordinates}>
+          <circle r={1} fill="#000000" strokeWidth={1} />
+          <text
+            textAnchor="middle"
+            y={markerOffset}
+            style={{ fontFamily: "system-ui", fill: "#EDEDED", "font-size": ".2rem" }}
+          >
+            {name}
+          </text>
+        </Marker>
+      ))}
       </ZoomableGroup>
     </ComposableMap>
   </div>

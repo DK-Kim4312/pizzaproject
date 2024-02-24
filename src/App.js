abdc43f09776b104ce34b3e278b6bac34dd668ec
Colin Luangrath <colinluangrath@gmail.com>
2024-02-24 12:46:33 -0600

    Added Simple Sidebar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { ComposableMap, Geographies, Geography } from "react-simple-maps";
-
+import Sidebar from './Sidebar/Sidebar';
 import "./index.css";
 
 const App = () => (
   <div>
+    <Sidebar />
     <ComposableMap>
       <Geographies geography="/features.json">
         {({ geographies }) =>

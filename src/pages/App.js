f39dd127634300845cc99b931edba6b6913a48ac
DK Kim <dkim693@wisc.edu>
2024-02-24 22:28:55 -0600

    add table

--- src/pages/App.js
+++ src/pages/App.js
@@ -6,15 +6,24 @@ import "../index.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Map from './Map';
 import States from './States';
+import Table from './Table';
+import { useParams } from "react-router-dom";
 
-const App = () => (
-  <BrowserRouter>
-    <Sidebar/>
-    <Routes>
-      <Route path="/" element={<Map/>}></Route>
-      <Route path="/United States" element={<States/>}></Route>
-    </Routes>
-  </BrowserRouter>
-);
 
+
+
+function App() {
+  const { id } = useParams();
+  return (
+    <BrowserRouter>
+      <Sidebar/>
+      <Routes>
+        <Route path="/" element={<Map/>}></Route>
+        <Route path="/United States" element={<States/>}></Route>
+        <Route path="/table" element={<Table/>}></Route>
+        <Route path="/:id" element={<Table country={id}/>}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
 export default App;
